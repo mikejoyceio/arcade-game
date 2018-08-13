0b8c7e24fc94b3f377f6a62f07e2cdd06f979d76
Mike Joyce <contact@mikejoyce.me>
2018-08-13 23:44:49 +0700

    Add player JS module

--- src/js/modules/game.js
+++ src/js/modules/game.js
@@ -15,8 +15,9 @@ import * as Helpers from 'helpers'
 import * as Music from 'music';
 import * as SFX from 'sfx';
 import { Enemy } from 'enemy';
-import { Gem } from 'gem';
 import { Engine } from 'engine';
+import { Gem } from 'gem';
+import { Player } from 'player';
 import { Resources } from 'resources';
 import { Stats } from 'stats';
 
@@ -235,139 +236,6 @@ export const Game = (function() {
   const gems = new Gems();
 
   /**
-   * @class Player - responsible for rendering the player, updating the
-   * player's position on the canvas and updating the player's lives.
-   * @constructor
-   */
-  const Player = function() {
-
-  	// Set the player's image
-  	this.sprite = 'dist/images/char-boy.png';
-
-  	// Set the player's x position on the canvas
-  	this.x = Constants.PLAYER_START_X;
-
-  	// Set the player's y position on the canvas
-  	this.y = Constants.PLAYER_START_Y;
-
-  	// Set the player's height
-  	this.height = Constants.ENTITY_HEIGHT;
-
-  	// Set the player's width
-  	this.width = Constants.ENTITY_WIDTH;
-
-  	/* Set the player's default lives.
-  	 * The player starts the game with 3.
-  	 */
-  	this.lives = 3;
-
-  };
-
-  /**
-   * update - update the player's position on Engine.canvas
-   * @memberOf Player
-   */
-  Player.prototype.update = function() {
-    this.xNow = this.x;
-    this.yNow = this.y;
-  };
-
-  /**
-   * reset - resets the players position on Engine.canvas
-   * @memberOf Player
-   */
-  Player.prototype.reset = function() {
-  	this.x = Constants.PLAYER_START_X;
-  	this.y = Constants.PLAYER_START_Y;
-  };
-
-  /**
-   * hit - called when the player collides with an enemy
-   * @memberOf Player
-   */
-  Player.prototype.hit = function() {
-    this.x = Constants.PLAYER_START_X;
-    this.y = Constants.PLAYER_START_Y;
-    $("#collision").show().fadeOut();
-    SFX.playerHit.play();
-  };
-
-  /**
-   * updateLives - updates the player's lives
-   * @memberOf Player
-   * @param  {string} action - add/remove
-   * @param  {number} value  - value to add/remove
-   */
-  Player.prototype.updateLives = function(action, value) {
-
-  	// Add a life
-  	if (action === 'add') {
-      this.lives = this.lives + value;
-  	}
-
-  	// Remove a life
-  	if (action === 'remove') {
-  	  this.lives = this.lives - value;
-  	}
-
-  	// Update the lives stats
-  	stats.updateLives(this.lives);
-
-  };
-
-  /**
-   * render - draw the player on Engine.canvas
-   * @memberOf Player
-   */
-  Player.prototype.render = function() {
-    Engine.ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-  };
-
-  /**
-   * handleInput - handle the left, up, right and down keyboard arrow keys input
-   * @memberOf Player
-   * @param  {number} key [description]
-   */
-  Player.prototype.handleInput = function(key) {
-
-  	/* If the left arrow key is pressed and the
-  	 * player is within the left boundary of the
-  	 * canvas, allow the player to go move left.
-  	 */
-  	if (key === 'left' && this.x != Constants.LEFT_BOUNDARY) {
-  	 this.x = this.xNow + -Constants.PLAYER_MOVEMENT;
-  	}
-
-  	/* If the up arrow key is pressed and the
-  	 * player is within the top boundary of the
-  	 * canvas, allow the player to move upwards.
-  	 */
-  	if (key === 'up' && this.y != Constants.TOP_BOUNDARY) {
-  	 this.y = this.yNow + -Constants.PLAYER_MOVEMENT;
-  	}
-
-  	/* If the right arrow key is pressed and the
-  	 * player is within the right boundary of the
-  	 * canvas, allow the player to move right.
-  	 */
-  	if (key === 'right' && this.x != Constants.RIGHT_BOUNDARY) {
-  	 this.x = this.xNow + Constants.PLAYER_MOVEMENT;
-  	}
-
-  	/* If the down arrow key is pressed and the
-  	 * player is within the bottom boundary of the
-  	 * canvas, allow the player to move down.
-  	 */
-  	if (key === 'down' && this.y != Constants.BOTTOM_BOUNDARY) {
-  	 this.y = this.yNow + Constants.PLAYER_MOVEMENT;
-  	}
-
-  };
-
-  // Instantiate new Player object
-  const player = new Player();
-
-  /**
    * @class Level - responsible for keeping track of and reseting the level
    * @constructor
    */
@@ -454,6 +322,9 @@ export const Game = (function() {
 
   // Instatiate a new Stats object
   const stats = new Stats();
+
+  // Instantiate new Player object
+  const player = new Player();
 
   /**
    * Listen for key presses and call the player.handleInput function if the game isn't paused.
