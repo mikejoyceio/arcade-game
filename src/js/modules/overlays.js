b0e710e845911251aed71098f17d47914df0625e
Mike Joyce <contact@mikejoyce.me>
2018-08-17 01:36:30 +0700

    Add overlays JS module

---
+++ src/js/modules/overlays.js
@@ -0,0 +1,12 @@
+/**
+ * @file Overlays
+ * @author Mike Joyce [hello@mikejoyce.io]
+ */
+
+import { Overlay } from 'overlay';
+
+export const intro = new Overlay('startScreen');
+
+export const gameOver = new Overlay('gameOver');
+
+export const howTo = new Overlay('howTo');
